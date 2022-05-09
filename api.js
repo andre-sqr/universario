892a8e26596527051f789080e8d1a3e7b5bd1631
andre-sqr <andrebsqr@gmail.com>
2022-05-09 17:48:11 -0300

    versão inicial da API implementada

---
+++ api.js
@@ -0,0 +1,34 @@
+// Requisição de API
+
+$('#submit').click(function (e){
+    e.preventDefault()
+    var dateInput = $('#birthdayInput').val()
+    $.ajax({
+    url: `https://api.nasa.gov/planetary/apod?api_key=pYIbv70Np8uaQJDIZumLpNB2oOJWcChnW7gwsgk9&date=${dateInput}`,
+    success: function (answer) {
+        if (answer.media_type == "video") {
+            console.log('entrou')
+            console.log(answer)
+            showsVid(answer)
+        } else {
+            showsImg(answer)
+        }
+    }
+})
+})
+
+function showsImg(answer) {
+    $('#imgDiv').css('display', 'flex')
+    $('#vidDiv').css('display', 'none')
+    $('#img-api').attr("src", answer.url)
+    $('#container-media_h2').text(answer.title)
+    $('#container-media_p').text(answer.explanation)
+}
+
+function showsVid(answer) {
+    $('#vidDiv').css('display', 'flex')
+    $('#imgDiv').css('display', 'none')
+    $('#vid-api').attr("src", answer.url)
+    $('#container-media_h2').text(answer.title)
+    $('#container-media_p').text(answer.explanation)
+}
